3f4d2d59a274dda580bb6cc270aa69545b4a662e
ramireztolosasantino <102975009+ramireztolosasantino@users.noreply.github.com>
2022-07-07 21:46:14 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+  stdio_init_all();
+  gpio_init(25);
+  gpio_set_dir(25, true);
+  while (true) {
+    gpio_put(25, true);
+    sleep_ms(500);
+    gpio_put(25, false);
+    sleep_ms(500);
+  }
+}
